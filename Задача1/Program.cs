a4c7d03e3c5d607a44b058a0830e92a21431dcf6
Katerina <m-a-c-u-k-a@mail.ru>
2022-12-26 22:06:27 +0700

    Задача1

---
+++ Задача1/Program.cs
@@ -0,0 +1,60 @@
+﻿//Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+int[,] numbers = new int[rows, columns];
+Fill2DIntArrayRandomNumbers(numbers);
+Write2DIntArray(numbers);
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        for (int k = 0; k < numbers.GetLength(1)-1; k++)
+        {
+            if (numbers[i, k] < numbers[i, k + 1])
+            {
+                int temp = numbers[i, k + 1];
+                numbers[i, k + 1] = numbers[i, k];
+                numbers[i, k] = temp;
+            }
+        }
+    }
+}
+Console.WriteLine();
+Write2DIntArray(numbers);
+
+
+
+
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Fill2DIntArrayRandomNumbers(int[,] array, int min = 1, int max = 10)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+}
+
+void Write2DIntArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
